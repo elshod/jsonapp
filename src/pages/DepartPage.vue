9d0b53cb21d1df364ec158992e1285d2a68ac8de
elshod <elshod5f@gmail.com>
2022-10-04 21:15:47 +0500

    bolim sahifasini to'g'irladim

--- src/pages/DepartPage.vue
+++ src/pages/DepartPage.vue
@@ -1,23 +1,43 @@
 <template>
     <div class="container">
         <div class="d-flex justify-content-between align-items-center">
-            <h3 class="text-center mt-3">Bo'limlar ro'yhati</h3>
+            <h3 class="text-center mt-3">
+                Bo'limlar ro'yhati
+            </h3>
             <div class="d-flex">
-                <input type="text" placeholder="Bo`lim nomi" class="form-control" v-model="depart.name">
-                <button class="ms-2 btn btn-success" @click="add()">Qo'shish</button>
+                <input 
+                    type="text" 
+                    placeholder="Bo`lim nomi" 
+                    class="form-control" 
+                    v-model="depart.name">
+                <button 
+                    class="ms-2 btn btn-success" 
+                    @click="add()">
+                    Qo'shish
+                </button>
             </div>
         </div>
         <table class="table">
-            <thead><tr><th>№</th><th>Bo'lim nomi</th><th></th></tr>
+            <thead>
+                <tr>
+                    <th>№</th>
+                    <th>Bo'lim nomi</th>
+                    <th></th>
+                </tr>
             </thead>
             <tbody v-if="departs.length>0">
                 <tr v-for="(d,i) of departs" :key="i">
-                    <td>{{i+1}}</td><td>{{d.name}}</td>
-                    <td><button @click="remove(d.id)" class="btn btn-danger">X</button></td>
+                    <td>{{i+1}}</td>
+                    <td>{{d.name}}</td>
+                    <td>
+                        <button @click="remove(d.id)" class="btn btn-danger">X</button>
+                    </td>
                 </tr>
             </tbody>
             <tbody v-else>
-                <tr><td colspan="3" class="text-center">Bo'limlar xozircha yo'q</td></tr>
+                <tr>
+                    <td colspan="3" class="text-center">Bo'limlar xozircha yo'q</td>
+                </tr>
             </tbody>
         </table>
     </div>
@@ -25,9 +45,12 @@
 <script>
 import axios from 'axios'
 export default {
-    data(){ return {
-        departs:[],depart:{}
-    }},
+    data(){ 
+        return {
+            departs:[],
+            depart:{}
+        }
+    },
     methods: {
         remove(id){
             if (confirm('Qaroringiz qat`iymi?')){
--- src/router.js
+++ src/router.js
@@ -3,14 +3,17 @@ import HomePage from '@/pages/HomePage'
 import DepartPage from '@/pages/DepartPage'
 import NewsPage from '@/pages/NewsPage'
 import ProjectsPage from '@/pages/ProjectsPage'
+
 const routes = [
     {path: '/',component: HomePage},
     {path: '/depart',component: DepartPage},
     {path: '/news',component: NewsPage},
     {path: '/projects',component: ProjectsPage},
 ]
+
 const router = createRouter({
     history: createWebHistory(),
     routes
 })
+
 export default router
